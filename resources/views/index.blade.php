d457998f1f5adcd727d899f4eab71a318be8ad49
Azhan <azhanmalick10@gmail.com>
2022-06-23 02:51:14 -1000

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>webTrendz</title>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="main" id="home">
+        <div class="bg">
+            <nav>
+                <h1>webTrendz <br> <p class="fs-6 text-center">It solutions</p></h1>
+                <ul id="list">
+                    <h3 class="inner">Webtrendz</h3>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">What we do</a></li>
+                    <li><a href="#services">Our services</a></li>
+                    <li><a href="#features">Features</a></li>
+                    <li><a href="#contact">Start your project</a></li>
+                </ul>
+                <button class="btn-sm login btn-success"><a href="#contact" class="text-decoration-none text-white">CONTACT US</a></button>
+                <div class="span">
+                    <h3 class="text-white cursor-pointer" onclick="myfunction()">&#9776;</h3>
+                </div>
+                
+            </nav>
+
+            <div class="img-2">
+                <img src="images/home-svg-2.svg" alt="">
+            </div>
+            
+            <div class="container">
+                <h1>We help leaders grow their <br> businesses for <span>a brighter future</span></h1>
+                <p>we are just 17 years mini developers but we have built many amazing projects and  <br> webapp we are pretty to start this  lets grow your business for a brigter feature technology <br> we improve ourself for a good site projects.</p>
+                <div class="buttons">
+                    <button class="btn my bg-white p-3 "><a href="#services" class="text-decoration-none text-secondary">Explore our services</a></button>
+                    <button class="btn text-white "><a href="#contact" class="text-decoration-none text-white">Discuss a project?</a></button>
+                </div>
+                <div class="img">
+                    <img src="images/home-svg-1.svg" alt="">
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+
+        <div class="controller"  id="about" data-aos="fade-up" data-aos-delay="50" data-aos-easing="zoom-in-left">
+            <div class="container d-flex align-items-center justify-center">
+                <div class="row">
+                    <h2 class="text-dark">We provide our expert services <span>around the world</span></h2>
+                    <p class="text-dark">Hey! you are in the correct platform in which you can make your own website we help you to grow your business low to high <br> we provide a high security first second step making website responsive and get your project ready to deploy.</p>
+                    <div class="btns">
+                        <button class="btn btn-success m-2">1. Security</button>
+                        <button class="btn btn-success m-2">2. fast service</button>
+                        <button class="btn btn-success m-2">3. logo desigining</button>
+                        <button class="btn btn-success m-2">4. web designing</button>
+                        <button class="btn btn-success m-2">5. mobile friendly</button>
+                    </div>
+                </div>
+                <div class="col">
+                    <img src="images/we-do.svg" alt="">
+                </div>
+            </div>
+
+            <div class="container c-2 d-flex align-items-center justify-center flex-col-reverse">
+                <div class="col">
+                    <img src="images/we-do-2.svg" alt="">
+                </div>
+                
+                <div class="row">
+                    <h2 class="text-dark">About our scope for web development <span>, wesite design</span></h2>
+                    <p class="text-dark">first of all we are new to webtrendz and want to develop new side project daily. we are only 17 years old but we have a skill to show you people so we started webtrendz so we are here to create you a beautiful responsive website we try to improve our skills day by day so lets start together.</p>
+                    
+                </div>
+                
+            </div>
+
+        </div>
+
+        <div class="container bg-dark p-4" id="services" data-aos="fade-up" data-aos-delay="50" data-aos-easing="ease-in-out"  data-aos-anchor-placement="top-center">
+            <p>Our services</p>
+            <h2>Our agency experts will provide you with a <br> <span>full stack of services</span></h2>
+            <p>Quis eu at condimentum amet sed facilisi viverra. Sit fames sed vulputate auctor tincidunt. Et sit <br> blandit eu nam scelerisque lorem lectus nibh dis.</p>
+            <div class="handler d-flex align-items-center justify-center flex-wrap">
+                
+    
+                <div class="card" data-aos="fade-up" data-aos-delay="10" data-aos-easing="ease-in-out"  data-aos-anchor-placement="top-center">
+                    <div class="shape">
+                        <img src="images/ser-2.svg" alt="">
+                    </div>
+                    <h6>SEO optimaization</h6>
+                    beautiful modern website design <br> with own created illustrations icons</p>
+                </div>
+
+                <div class="card">
+                    <div class="shape" data-aos="fade-up" data-aos-delay="20" data-aos-easing="ease-in-out"  data-aos-anchor-placement="top-center">
+                        <img src="images/ser-3.svg" alt="">
+                    </div>
+                    <h6>Desgning</h6>
+                    <p>beautiful modern website design <br> with own created illustrations icons</p>
+                </div>
+
+                <div class="card" data-aos="fade-up" data-aos-delay="30" data-aos-easing="ease-in-out"  data-aos-anchor-placement="top-center">
+                    <div class="shape">
+                        <img src="images/ser-4.svg" alt="">
+                    </div>
+                    <h6>Development</h6>
+                    <p>we are very glad to take first order <br> trying to make a new skill</p>
+                </div>
+
+                <div class="card" data-aos="fade-up" data-aos-easing="ease-in-out"  data-aos-anchor-placement="top-center">
+                    <div class="shape">
+                        <img src="images/ser-5.svg" alt="">
+                    </div>
+                    <h6>Research</h6>
+                    <p>we are shore about we will bring some new changes <br> a modern design</p>
+                </div>
+
+               
+
+            </div>
+
+        </div>
+
+        <div class="container mynone bg-dark p-5" data-aos="fade-up" data-aos-delay="30" data-aos-easing="ease-in-out"  data-aos-anchor-placement="top-center">
+            <div class="img">
+                <img src="images/home-svg-2.svg" alt="">
+            </div>
+            <h2>Hey! Can we help your <br> product with some of our expert services?</h2>
+            <button class="btn btn-success m-3">Lets discuss</button>
+        </div>
+
+        <div class="container feat" id="features" data-aos="fade-up" data-aos-delay="30" data-aos-easing="ease-in-out"  data-aos-anchor-placement="top-center">
+            <div>
+                <p>FEATURES</p>
+                <h2><span>We care</span> about profits of your <br> business</h2>
+                <p>we never fells about to loss your business we are here to help you and get started your website today<br> you have made website from all developers but try expirience with a 17 years mini developer. what services we provide for you?</p>
+            </div>
+            <div class="cards d-flex p-5 ope">
+                <div class="whole">
+                    <img src="images/care-1.svg" alt="">
+                    <h4>Operations</h4>
+                    <p>we are ready to make any kind of website that user asks</p>
+                </div>
+    
+                <div class="whole" data-aos="fade-up" data-aos-delay="10" data-aos-easing="ease-in-out"  data-aos-anchor-placement="top-center">
+                    <img src="images/care-2.svg" alt="">
+                    <h4>Quality</h4>
+                    <p>you can see a quality in our work lets start your project</p>
+                </div>
+    
+                <div class="whole" data-aos="fade-up" data-aos-delay="10" data-aos-easing="ease-in-out"  data-aos-anchor-placement="top-center">
+                    <img src="images/care-3.svg" alt="">
+                    <h4>responsive</h4>
+                    <p>make a website for cheap price as well you get your website responsive</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container d-flex align-items-center justify-between bg-dark p-5" data-aos="fade-up" data-aos-delay="30" data-aos-easing="ease-in-out"  data-aos-anchor-placement="top-center">
+            <h4 class="text-white m-4">Ready to discuss your rock star projects? Let’s discuss it</h4>
+            <button class="btn btn-success">Contact us</button>
+        </div>
+
+        <div class="container d-flex align-items-center justify-center">
+            <div class="">
+                <p class="text-secondary">OUR SUCCESS IN NUMBERS</p>
+                <h3 class="text-dark">Our work didn’t go unnoticed</h3>
+                <p class="text-secondary">we are sure about our quality service we try to improve ourself we are new to this but we have an expirience and master in computer science .</p>
+            </div>
+            <div class="">
+                <img src="images/success.svg" alt="">
+            </div>
+            
+        </div>
+
+        <div class="container contact d-flex align-items-center justify-center" id="contact" data-aos="fade-up" data-aos-delay="10" data-aos-easing="ease-in-out"  data-aos-anchor-placement="top-center">
+            <div class="contact-img">
+                
+                
+                <img src="images/contact.svg" alt="">
+                @if(Session::has('store'))
+        <p class="alert alert-success">{{ Session::get('store') }}</p>
+    @endif
+            </div>
+            <div class="text">
+                <p class="text-secondary">CONTACT OUR EXPERT</p>
+                <h2 class="text-dark">Ready to discuss project?
+                    ‍Drop us a line</h2>
+                    <form action="{{ route('create' ) }}" method="POST">
+                        @csrf
+                        <div class="inputs">
+                            <input type="text" placeholder="Your name" name="name">
+                        </div>
+
+                        <div class="inputs">
+                            <input type="number" placeholder="mobile no" name="mobile">  {{--this want to be corrected --}}
+                        </div>
+
+                        <div class="inputs">
+                            <input type="text" placeholder="Type your email" name="email">
+                        </div>
+
+                        <div class="inputs">
+                            <input type="text" placeholder="Ask your question" name="question">
+                        </div>
+                        <button type="submit" class="btn btn-success">Send a message</button>
+                    </form>
+            </div>
+        </div>
+
+        <div class="container foot d-flex align-items-center justify-content-around flex-column  m-5 p-5">
+            <div class="logo-sm">
+               <h3 class="text-dark"> webTrendz</h3>
+               <p>created by azhan</p>
+            </div>
+            <div class="list" class="text-dark ">
+                <a class="text-decoration-none m-3 text-dark" href="about">What we do</a>
+                <a class="text-decoration-none m-3 text-dark" href="#services">Our services</a>
+                <a class="text-decoration-none m-3 text-dark" href="#features">Features</a>
+                {{-- <a class="text-decoration-none m-3 text-dark" href="#" class="disabled">Solutions</a> --}}
+                <a class="text-decoration-none m-3 text-dark" href="#contact">Contact</a>
+            </div>
+            <div>
+
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="container extra d-flex align-items-center justify-content-around flex-column">
+           
+            <div>
+                <p>ALL RIGHTS RESERVED 2022</p>
+            </div>
+        </div>
+
+        <script>
+            function myfunction(){
+               list = document.getElementById('list')
+               if(list.style.display == "none"){
+                list.style.display = "block"
+               }
+               else{
+                list.style.display = "none";
+               }
+            }
+
+        </script>
+
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+    
+
+
+</body>
+</html>
